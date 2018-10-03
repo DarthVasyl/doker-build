442ede26e196f9b376ef9d92fa21de18c5eda564
Vasyl Doroshenko <formicking@gmail.com>
2018-10-03 22:45:02 +0300

    comment redis

--- container/install_moodle.sh
+++ container/install_moodle.sh
@@ -18,20 +18,20 @@
     $(if [ $INIT_DB -eq 0 ]; then echo "--skip-database"; fi)
 
 # add Redis configuration
-sed -i '/^\s*require_once/i\\$CFG->session_redis_host = '"'${REDIS_SERVER}';" /var/www/html/moodle/config.php
-sed -i '/^\s*require_once/i\\$CFG->session_handler_class = '"'"'\\core\\session\\redis'"'"'\;' /var/www/html/moodle/config.php
-sed -i '/^\s*require_once/i\\$CFG->session_redis_acquire_lock_timeout = 120;' /var/www/html/moodle/config.php
-sed -i '/^\s*require_once/i\\$CFG->session_redis_lock_expire = 7200;' /var/www/html/moodle/config.php
-sed -i '/^\s*require_once/i\\$CFG->session_redis_database = 0;' /var/www/html/moodle/config.php
-sed -i '/^\s*require_once/i\\$CFG->session_redis_port = 6379;' /var/www/html/moodle/config.php
+#sed -i '/^\s*require_once/i\\$CFG->session_redis_host = '"'${REDIS_SERVER}';" /var/www/html/moodle/config.php
+#sed -i '/^\s*require_once/i\\$CFG->session_handler_class = '"'"'\\core\\session\\redis'"'"'\;' /var/www/html/moodle/config.php
+#sed -i '/^\s*require_once/i\\$CFG->session_redis_acquire_lock_timeout = 120;' /var/www/html/moodle/config.php
+#sed -i '/^\s*require_once/i\\$CFG->session_redis_lock_expire = 7200;' /var/www/html/moodle/config.php
+#sed -i '/^\s*require_once/i\\$CFG->session_redis_database = 0;' /var/www/html/moodle/config.php
+#sed -i '/^\s*require_once/i\\$CFG->session_redis_port = 6379;' /var/www/html/moodle/config.php
 
 # Change Apache configs
 sed -i 's#AllowOverride [nN]one#AllowOverride All#' /etc/httpd/conf/httpd.conf
 sed -i 's#DirectoryIndex index.html#DirectoryIndex index.php#' /etc/httpd/conf/httpd.conf
 
 # Enable Redis in PHP
-echo "session.save_handler = redis" >> /etc/php.d/redis.ini
-echo "session.save_path = \"tcp://${REDIS_SERVER}:6379\"" >> /etc/php.d/redis.ini
+#echo "session.save_handler = redis" >> /etc/php.d/redis.ini
+#echo "session.save_path = \"tcp://${REDIS_SERVER}:6379\"" >> /etc/php.d/redis.ini
 
 # Make apache owner of moodle files
 chown -R apache:apache /var/moodledata

0cc4b060be94d7b9d6c5c935b820157aa040b479
Vasyl Doroshenko <formicking@gmail.com>
2018-10-05 00:07:46 +0300

    edit script

--- scripts/dm.sh
+++ scripts/dm.sh
@@ -1,2 +1,8 @@
-sudo gcloud beta deployment-manager deployments create test  --config k8-deployments/demo.yaml \
+# create kubernetes cluster with deployment manager
+sudo gcloud beta deployment-manager deployments create cluster  --config ../deployment-manager/cluster.yaml \
 --project=gcloud-docker-demo --create-policy CREATE
+
+# create deployment for cluster
+sudo gcloud beta deployment-manager deployments create moodle  --config ../deployment-manager/moodle.yaml \
+--project=gcloud-docker-demo --create-policy CREATE
+
